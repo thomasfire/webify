5d70c94f2ba5b1701f721792f921cdc16ed5e1d8
thomasfire <tomasfire@live.com>
2021-09-12 21:47:10 +0300

    Proper redis run
    Signed-off-by: thomasfire <tomasfire@live.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-redis-server --port 6381 --daemonize yes --save "" --appendonly no
-redis-server --port 6380 --daemonize yes
+redis-server --port 6381 --daemonize yes --save "" --appendonly no --bind 127.0.0.1
+redis-server --port 6380 --daemonize yes --bind 127.0.0.1
 RUST_LOG=webify=trace,actix_web=info,actix_server=info ./webify
